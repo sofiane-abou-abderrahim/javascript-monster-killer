c99856d1ecd544914c7fe89b59c7f242d8077acf
Sofiane Abou Abderrahim <sofianeabouabderrahim@gmail.com>
2023-09-03 16:38:37 +0300

    added if statements for checking win/lose/draw conditions copied from source file

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,5 +1,4 @@
 const ATTACK_VALUE = 10;
-const STRONG_ATTACK_VALUE = 17;
 const MONSTER_ATTACK_VALUE = 14;
 
 let chosenMaxLife = 100;
@@ -8,14 +7,8 @@ let currentPlayerHealth = chosenMaxLife;
 
 adjustHealthBars(chosenMaxLife);
 
-function attackMonster(mode) {
-  let maxDamage;
-  if (mode === 'ATTACK') {
-    maxDamage = ATTACK_VALUE;
-  } else if (mode === 'STRONG_ATTACK') {
-    maxDamage = STRONG_ATTACK_VALUE;
-  }
-  const damage = dealMonsterDamage(maxDamage);
+function attackHandler() {
+  const damage = dealMonsterDamage(ATTACK_VALUE);
   currentMonsterHealth -= damage;
   const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
   currentPlayerHealth -= playerDamage;
@@ -23,18 +16,9 @@ function attackMonster(mode) {
     alert('You won!');
   } else if (currentPlayerHealth <= 0 && currentMonsterHealth > 0) {
     alert('You lost!');
-  } else if (currentMonsterHealth <= 0 && currentPlayerHealth <= 0) {
+  } else if (currentPlayerHealth <= 0 && currentMonsterHealth <= 0) {
     alert('You have a draw!');
   }
 }
 
-function attackHandler() {
-  attackMonster('ATTACK');
-}
-
-function strongAttackHandler() {
-  attackMonster('STRONG_ATTACK');
-}
-
 attackBtn.addEventListener('click', attackHandler);
-strongAttackBtn.addEventListener('click', strongAttackHandler);
