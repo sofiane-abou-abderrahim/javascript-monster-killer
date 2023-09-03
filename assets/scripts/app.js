8847a028025d9b75a1e4798112a3f8bbbcf4f6a9
Sofiane Abou Abderrahim <sofianeabouabderrahim@gmail.com>
2023-09-03 18:55:32 +0300

    added enteredValue variable and if statement checking for isNaN

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -3,7 +3,14 @@ const STRONG_ATTACK_VALUE = 17;
 const MONSTER_ATTACK_VALUE = 14;
 const HEAL_VALUE = 20;
 
-let chosenMaxLife = 100;
+const enteredValue = prompt('Maximum life for you and the monster.', '100');
+
+let chosenMaxLife = parseInt(enteredValue);
+
+if (isNaN(chosenMaxLife) || chosenMaxLife <= 0) {
+  chosenMaxLife = 100;
+}
+
 let currentMonsterHealth = chosenMaxLife;
 let currentPlayerHealth = chosenMaxLife;
 let hasBonusLife = true;
