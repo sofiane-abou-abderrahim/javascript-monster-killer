b34f400afb9412ce381ca2dff0b8ec8340399f47
Sofiane Abou Abderrahim <sofianeabouabderrahim@gmail.com>
2023-09-03 18:26:31 +0300

    added reset function

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -10,6 +10,12 @@ let hasBonusLife = true;
 
 adjustHealthBars(chosenMaxLife);
 
+function reset() {
+  currentMonsterHealth = chosenMaxLife;
+  currentPlayerHealth = chosenMaxLife;
+  resetGame(chosenMaxLife);
+}
+
 function endRound() {
   const initialPlayerHealth = currentPlayerHealth;
   const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
@@ -29,6 +35,10 @@ function endRound() {
     alert('You lost!');
   } else if (currentMonsterHealth <= 0 && currentPlayerHealth <= 0) {
     alert('You have a draw!');
+  }
+
+  if (currentMonsterHealth <= 0 || currentPlayerHealth <= 0) {
+    reset();
   }
 }
 
