8000d6f56f6018581065d4fc8627b97214522fec
Sofiane Abou Abderrahim <sofianeabouabderrahim@gmail.com>
2023-09-06 17:57:44 +0300

    added try & catch to getMaxLifeValues function

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -25,7 +25,16 @@ function getMaxLifeValues() {
   return parsedValue;
 }
 
-let chosenMaxLife = getMaxLifeValues();
+let chosenMaxLife;
+
+try {
+  chosenMaxLife = getMaxLifeValues();
+} catch (error) {
+  console.log(error);
+  chosenMaxLife = 100;
+  alert('You entered something wrong, default value of 100 was used.');
+  // throw error;
+}
 
 let currentMonsterHealth = chosenMaxLife;
 let currentPlayerHealth = chosenMaxLife;
